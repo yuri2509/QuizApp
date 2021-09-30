fa264144953556620b1c31f23bd8b85b0dbb1dd7
yuri2509 <tukine.doll.love@gmail.com>
2021-09-30 13:40:45 +0900

    タブルタップを防ぐ

--- QuizApp/QuizViewController.swift
+++ QuizApp/QuizViewController.swift
@@ -52,8 +52,16 @@ class QuizViewController: UIViewController {
         }
         print("スコア:\(correctCount)")
         judgeImageView.isHidden = false
+        answerButton1.isEnabled = false
+        answerButton2.isEnabled = false
+        answerButton3.isEnabled = false
+        answerButton4.isEnabled = false
         DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
             self.judgeImageView.isHidden = true
+            self.answerButton1.isEnabled = true
+            self.answerButton2.isEnabled = true
+            self.answerButton3.isEnabled = true
+            self.answerButton4.isEnabled = true
             self.nextQuiz()
         }
     }
